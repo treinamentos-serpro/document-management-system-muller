88edb96a7edd8270858b224d059cca395234b6d4
vdmuller <vdmuller@gmail.com>
2026-09-15 17:53:09 +0000

    refactor: remove unused components (DocumentList, DownloadButton, UploadComponent)

--- frontend/src/components/DownloadButton.jsx
+++
@@ -1,7 +0,0 @@
-export default function DownloadButton({ document, onDownload }) {
-  return (
-    <button className="download-button" type="button" onClick={() => onDownload(document)}>
-      Baixar
-    </button>
-  );
-}
